6bb1e591a5c53681e59fc84e67b1e73280757548
Bee <ardelmbiplang@gmail.com>
2020-01-16 12:21:40 +0100

    code review

--- server.js
+++ server.js
@@ -48,45 +48,7 @@ app.post('/signin', (req, res) => {
 
 })
 
-app.post('/register', (req, res) => {
-
-    const { email, name, password } = req.body
-
-    const hash = bcrypt.hashSync(password);
-
-   
-    db.transaction(trx => {
-        trx.insert({
-            hash,
-            email
-        }).into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0],
-                    name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0])
-                })
-                .then(trx.commit)
-                .catch(trx.rollback)
-}).catch(error => res.status(400).json('unable to register!'))
-
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id })
-    .then(user => {
-        user.length 
-        ? res.status(200).json(user[0])
-        : res.status(404).json('Not Found')
-    })
-    .catch(error => res.status(400).json('Error getting user!'))
-})
+app.post('/register', )
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
@@ -99,11 +61,6 @@ app.put('/image', (req, res) => {
         res.json(entries[0])
     })
     .catch(error => res.status(400).json('unable to get entrie(s)'))
-        })
-    })
-
-
-    
 })
 
 
