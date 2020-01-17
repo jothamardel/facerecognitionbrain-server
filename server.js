ecafda1d93cd3efaee61c1d1da6103adbd2d97d0
Bee <ardelmbiplang@gmail.com>
2020-01-17 17:55:17 +0100

    Environmental Variable (process.env.port) update

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res) })
 
 
+let PORT = process.env.PORT 
 
-
-app.listen(3000, () => {
-    console.log("app is running on port 3000")
+app.listen(PORT || 3000, () => {
+    console.log(`app is running on port ${PORT}`)
 }) 
 
 
