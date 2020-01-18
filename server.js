21abc78ecfcaa344067ce2021c145e7baa3ade8e
Bee <ardelmbiplang@gmail.com>
2020-01-18 11:19:40 +0100

    Connecting postgres to node

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const users = require('./controllers/users')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'jotham',
-      password : 'topimpabutterfly',
-      database : 'smartbrain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
